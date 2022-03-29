f46f0017385e0073fd0ff5b8401f54a49fb86a3d
Gustavo Anjos do Vale <gustavoanjos13@hotmail.com>
2022-03-28 22:09:46 -0300

    primeiro commit

---
+++ src/projeto_bdii/Projeto_BDII.java
@@ -0,0 +1,12 @@
+package projeto_bdii;
+
+import View.Principal;
+
+public class Projeto_BDII {
+
+    public static void main(String[] args) {
+        Principal jp = new Principal();
+        jp.setVisible(true);
+    }
+    
+}
